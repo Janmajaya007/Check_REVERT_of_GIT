33a6a5cd73a3fbd2930915d9bb5fde410813e905
janmajaya007 <janmajaya2000@gmail.com>
2023-07-23 14:52:20 +0530

    adding file to check REVERT command

---
+++ Cognizant_java/src/Revert_Check.java
@@ -0,0 +1,11 @@
+/**
+ * @author Janmajaya
+ * @project Check_REVERT_of_GIT
+ */
+public class Revert_Check {
+    public static void main(String[] args) {
+        System.out.println("Hello Janmajaya");
+        System.out.println("Checking for REVERT Command");
+
+    }
+}
